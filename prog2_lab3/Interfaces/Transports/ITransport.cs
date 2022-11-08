86ddb052c28ca006b68c76a717ff08cace086920
Boomrock <63188345+Boomrock@users.noreply.github.com>
2022-11-08 11:30:28 +0700

    Добавьте файлы проекта.

---
+++ prog2_lab3/Interfaces/Transports/ITransport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prog2_lab3.Interfeses
+{
+    interface ITransport
+    {
+        float Cost { get; set; }
+
+        string Path { get; set; }
+    }
+}
